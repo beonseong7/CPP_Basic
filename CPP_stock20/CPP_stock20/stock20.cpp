8f4d61deef07800ae69dd385556c5480d033dccf
beonseong7 <beonseong7@naver.com>
2022-07-17 21:17:12 +0900

    2022.07.17 about improved class2

---
+++ CPP_stock20/CPP_stock20/stock20.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include"stock20.h"
+
+Stock::Stock()
+{
+	company = "no name";
+	shares = 0;
+	share_val = 0.0;
+	total_val = 0.0;
+}
+Stock::Stock(const std::string& co, long n, double pr)
+{
+	company = co;
+	if (n < 0)
+	{
+		std::cout << "주식 수는 음수가 될 수 없으므로, "
+			<< company << " shares를 0으로 설정합니다.\n";
+		shares = 0;
+	}
+	else
+		shares = n;
+	share_val = pr;
+	set_tot();
+}
+Stock::~Stock()
+{
+}
+void Stock::buy(long num, double price)
+{
+	if (num < 0)
+	{
+		std::cout << "매입 주식 수는 음수가 될 수 없으므로,"
+			<< "거래가 취소되었습니다.\n";
+	}
+	else
+	{
+		shares += num;
+		share_val = price;
+		set_tot();
+	}
+}
+void Stock::sell(long num, double price)
+{
+	using std::cout;
+	if (num < 0)
+	{
+		cout << "매도 주식 수는 음수가 될 수 없으므로, "
+			<< "거래가 취소되었습니다.\n";
+	}
+	else if (num > shares)
+	{
+		cout << "보유 주식보다 많은 주식을 매도 할 수 없으므로,"
+			<< "거래가 취소되었습니다.\n";
+	}
+	else 
+	{
+		shares -= num;
+		share_val = price;
+		set_tot();
+	}
+}
