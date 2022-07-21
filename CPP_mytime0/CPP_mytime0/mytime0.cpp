6d677e38dcef4f8864b85e2450c3778a1685055a
beonseong7 <beonseong7@naver.com>
2022-07-21 21:30:44 +0900

    2022.07.21 overloding class

---
+++ CPP_mytime0/CPP_mytime0/mytime0.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include"mytime0.h"
+
+Time::Time()
+{
+	hours = minutes = 0;
+}
+Time::Time(int h, int m)
+{
+	hours = h;
+	minutes = m;
+}
+void Time::AddMin(int m)
+{
+	minutes += m;
+	hours += minutes / 60;
+	minutes %= 60;
+}
+void Time::AddHr(int h)
+{
+	hours += h;
+}
