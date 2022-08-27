896db505153e98672feac97c73f81f6fc3dd3abf
beonseong7 <beonseong7@naver.com>
2022-08-27 15:42:55 +0900

    2022.08.27 Abstract Base Class

---
+++ CPP_acctabc/CPP_acctabc/acctabc.h
@@ -0,0 +1,37 @@
+#ifndef ACCTABC_H_
+#define ACCTABC_H_
+#include <iostream>
+#include <string>
+
+class AcctABC
+{
+private:
+	std::string fullName;
+	long acctNum;
+	double balance;
+protected:
+	struct Formatting
+	{
+		std::ios_base::fmtflags flag;
+		std::streamsize pr;
+	};
+	const std::string& FullName() const { return fullName; }
+	long AccNum() const { return acctNum; }
+	Formatting setFormat() const;
+	void Restore(Formatting& f) const;
+public:
+	AcctABC(const std::string& s = "NullBody", long an = -1, double bal = 0.0);
+	void Deposit(double amt);
+	virtual void Withdraw(double amt) = 0;
+	double Balance() const { return balance; };
+	virtual void ViewAcct() const = 0;
+	virtual ~AcctABC() {}
+};
+class Brass :public AcctABC
+{
+public:
+	Brass(const std::string& s = "Nullbody", long an = -1, double bal = 0.0) : AcctABC(s, bal) {}
+	virtual void Withdraw(double amt);
+	virtual void ViewAcct() const;
+	virtual ~Brass() {}
+};
