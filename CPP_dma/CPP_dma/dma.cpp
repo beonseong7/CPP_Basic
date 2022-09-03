7c6111909c110f44e447327194a868527b8c9ff7
beonseong7 <beonseong7@naver.com>
2022-09-03 20:32:12 +0900

    2022.09.03 Dynamic Memory and Friends

--- CPP_dma/CPP_dma/dma.cpp
+++ CPP_dma/CPP_dma/dma.cpp
@@ -27,3 +27,37 @@ baseDMA& baseDMA::operator=(const baseDMA& rs)
 	rating = rs.rating;
 	return *this;
 }
+std::ostream& operator << (std::ostream& os, const baseDMA& rs)
+{
+	os << "상표: " << rs.label << std::endl;
+	os << "등급: " << rs.rating << std::endl;
+	return os;
+}
+lacksDMA::lacksDMA(const char* c, const char* l, int r):baseDMA(l,r)
+{
+	std::strncpy(color, c, COL_LEN - 1);
+	color[39] = '\0';
+}
+lacksDMA::lacksDMA(const char* c, const baseDMA& rs) : baseDMA(rs)
+{
+	std::strncpy(color, c, COL_LEN - 1);
+	color[COL_LEN - 1] = '\0';
+}
+std::ostream& operator<<(std::ostream& os, const lacksDMA& ls) 
+{
+	os << (const baseDMA&)ls;
+	os << "색상:" << ls.color << std::endl;
+	return os;
+}
+hasDMA::hasDMA(const char* s, const char* l, int r)
+	:baseDMA(l, r)
+{
+	style = new char[std::strlen(s) + 1];
+	std::strcpy(style, s);
+}
+hasDMA::hasDMA(const char* s, const baseDMA& rs)
+	: baseDMA(rs)
+{
+	style = new char[std::strlen(s) + 1];
+	std::strcpy(style, s);
+}
