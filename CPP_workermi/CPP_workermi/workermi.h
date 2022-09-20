b2ac00f4d78699fdc135ca528173b526444163d2
beonseong7 <beonseong7@naver.com>
2022-09-20 20:40:03 +0900

    2022.09.20 virtual basic class

--- CPP_workermi/CPP_workermi/workermi.h
+++ CPP_workermi/CPP_workermi/workermi.h
@@ -24,5 +24,40 @@ class Waiter : virtual public Worker
 private:
 	int panache;
 protected:
+	void Data() const;
+	void Get();
+public:
+	Waiter() : Worker(), panache(0) {}
+	Waiter(const std::string& s, long n, int p = 0)
+		:Worker(s, n), panache(p) {}
+	Waiter(const Worker& wk, int p = 0)
+		:Worker(wk), panache(p) {}
+	void Set();
+	void Show() const;
+};
+class Singer : virtual public Worker
+{
+protected:
+	enum { other, alto, contralto, soprano, bass, baritone, tenor };
+	enum { Vtypes = 7 };
+	void Date() const;
+	void Get();
+private:
+	static char* pv[Vtypes];
+	int voice;
+public:
+	Singer() : Worker(), voice(other) {}
+	Singer(const std::string& s, long n, int v = other)
+		: Worker(s, n), voice(v) {}
+	Singer(const Worker& wk, int v = other)
+		: Worker(wk), voice(v) {}
+	void Set();
+	void Show() const;
+};
+class SingingWaiter : public Singer, public Waiter
+{
+protected:
+	void Date() const;
+	void Get();
 };
 #endif
