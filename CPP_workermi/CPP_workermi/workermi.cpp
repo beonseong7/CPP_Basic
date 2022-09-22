422665a96abad3e16186789fec41dbe2d6d5e324
beonseong7 <beonseong7@naver.com>
2022-09-22 20:13:21 +0900

    2022.09.22 virtual basic class

--- CPP_workermi/CPP_workermi/workermi.cpp
+++ CPP_workermi/CPP_workermi/workermi.cpp
@@ -13,4 +13,32 @@ void Worker::Data() const
 void Worker::Get()
 {
 	getline(cin, fullname);
+	cout << "사원 번호를 입력하십시오: ";
+	cin >> id;
+	while (cin.get() != '\n')
+		continue;
 }
+void Waiter::Set()
+{
+	cout << "웨이터의 이름을 입력하십시오: ";
+	Worker::Get();
+	Get();
+}
+void Waiter::Show() const
+{
+	cout << "직종: 웨이터\n";
+	Worker::Data();
+	Data();
+}
+void Waiter::Data() const
+{
+	cout << "웨이터 등급: " << panache << endl;
+}
+void Waiter::Get()
+{
+	cout << "웨이터 등급을 입력하십시오: ";
+	cin >> panache;
+	while (cin.get() != '\n')
+		continue;
+}
+char* Singer::pv[Singer::Vtypes] = { "other","alto","contralto","soprano","bass","baritone","tenor" };
