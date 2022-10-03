e3a0c6fb667a6ca3d733b2f29d90373f00f67fd0
beonseong7 <beonseong7@naver.com>
2022-10-03 18:11:37 +0900

    2022.10.3 stack template and dynamic array

--- CPP_stcktp1/CPP_stcktp1/stcktp1.h
+++ CPP_stcktp1/CPP_stcktp1/stcktp1.h
@@ -49,5 +49,22 @@ bool Stack<Type>::pop(Type& item)
 	if (top > 0)
 	{
 		item = items[--top];
+		return ture;
 	}
+	else
+		return false;
 }
+template<class Type>
+Stack<Type>& Stack<Type>::operator=(const Stack<Type>& st)
+{
+	if (this == &st)
+		return *this;
+	delete[] items;
+	stacksize = st.stacksize;
+	top = st.top;
+	items = new Type[stacksize];
+	for (int i = 0; i < top; i++)
+		items[i] = st.items[i];
+	return *this;
+}
+#endif
