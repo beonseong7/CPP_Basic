a6d347cb57aa42d9948ec464142ccbc3ee36f8e3
beonseong7 <beonseong7@naver.com>
2022-10-06 22:51:19 +0900

    2022.10.6 array template

--- CPP_arraytp/CPP_arraytp/arraytp.h
+++ CPP_arraytp/CPP_arraytp/arraytp.h
@@ -35,6 +35,10 @@ T arrayTP<T, n>::operator[](int i) const
 {
 	if (i < 0 || i >= n)
 	{
-		std::cerr<<"배열의 경계를 벗어나는 에러: "
+		std::cerr << "배열의 경계를 벗어나는 에러: "
+			<< i << "--> 잘못된 인덱스입니다.\n";
+		std::exit(EXIT_FAILURE);
 	}
+	return ar[i];
 }
+#endif
---
+++ CPP_arraytp/CPP_arraytp/twod.cpp
@@ -0,0 +1,3 @@
+#include<iostream>
+#include"arraytp.h"
+int main()
