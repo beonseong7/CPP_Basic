6481ef0f8f56cbd599bd1cc9c7a28cdbfcd1dbf8
beonseong7 <beonseong7@naver.com>
2022-10-10 15:29:24 +0900

    2022.10.9 array template

--- CPP_tempmemb/CPP_tempmemb/tempmemb.cpp
+++ CPP_tempmemb/CPP_tempmemb/tempmemb.cpp
@@ -20,5 +20,20 @@ private:
 	hold<int> n;
 public:
 	beta(T t, int i) :q(t), n(i) {}
-	template
+	template<typename U>
+	U blab(U u, T t) { return (n.Value() + q.Value()) * u / t; }
+	void Show() const { q.show(); n.show(); }
 };
+int main()
+{
+	beta<double> guy(3.5, 3);
+	cout << "T가 double로 설정되었습니다.\n";
+	guy.Show();
+	cout << "V가 double인 T로 설정되었고, 그 다음에 V가 int로 설정되었습니다.\n";
+	cout << guy.blab(10, 2.3) << endl;
+	cout << "U가 int로 설정되었습니다.\n";
+	cout << guy.blab(10.0, 2.3) << endl;
+	cout << "U가 double로 설정되었습니다.\n";
+	cout << "프로그램을 종료합니다.\n";
+	return 0;
+}
