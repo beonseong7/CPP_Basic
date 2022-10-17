2cd32b41f13bcbf912762d42b3bf046521aa93be
beonseong7 <beonseong7@naver.com>
2022-10-17 21:15:33 +0900

    2022.10.17 friend class

---
+++ CPP_tv/CPP_tv/tv.h
@@ -0,0 +1,32 @@
+#ifndef TV_H_
+#define TV_H_
+
+class Tv
+{
+public:
+	friend class Remote;
+	enum { Off, On };
+	enum { MinVal, MaxVal = 20 };
+	enum { Antenna, Cable };
+	enum { TV, DVD };
+
+	Tv(int s = Off, int mc = 125) : state(s), volume(5),
+		maxchannel(mc), channel(2), mode(Cable), input(TV) {}
+	void onoff() { state = (state == On) ? Off : On; }
+	bool ison() const { return state == On; }
+	bool volup();
+	bool voldown();
+	void chanup();
+	void chandonw();
+	void set_mode() { mode = (mode == Antenna) ? Cable : Antenna; }
+	void set_input() { input = (input == TV) ? DVD : TV; }
+	void settings() const;
+private:
+	int state;
+	int volume;
+	int maxchannel;
+	int channel;
+	int mode;
+	int input;
+};
+#endif
