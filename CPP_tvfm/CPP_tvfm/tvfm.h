12fb44657fa457a4fed283aed8bd53ea164d9681
beonseong7 <beonseong7@naver.com>
2022-10-22 17:18:21 +0900

    2022.10.22 friend member function

--- CPP_tvfm/CPP_tvfm/tvfm.h
+++ CPP_tvfm/CPP_tvfm/tvfm.h
@@ -28,6 +28,27 @@ public:
 class Tv
 {
 public:
+	friend void Remote::set_chan(Tv& t, int c);
+	enum State { Off, On };
+	enum { MinVal, MaxVal = 20 };
+	enum { Antenna, Cable };
+	enum { TV, DVD };
+	Tv(int s = Off, int mc = 125) : state(s), volume(5), maxchannel(mc), channel(2), mode(Cable), input(TV) {}
+	void onoff() { state = (state == On) ? Off : On; }
+	bool ison() const { return state == On; }
+	bool volup();
+	bool voldown();
+	void chanup();
+	void chandown();
+	void set_mode() { mode = (mode == Antenna) ? Cable : Antenna; }
+	void set_input() { input = (input == TV) ? DVD : TV; }
+	void settings() const;
 private:
+	int state;
+	int volume;
+	int maxchannel;
+	int channel;
+	int mode;
+	int input;
 };
 #endif
