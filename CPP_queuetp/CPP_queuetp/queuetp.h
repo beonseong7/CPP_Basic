45bdde3125cbe6575e670018e85f2bd396780420
beonseong7 <beonseong7@naver.com>
2022-10-24 20:34:37 +0900

    2022.10.24 Queue Template

--- CPP_queuetp/CPP_queuetp/queuetp.h
+++ CPP_queuetp/CPP_queuetp/queuetp.h
@@ -18,6 +18,40 @@ private:
 	const int qsize;
 	QueueTP(const QueueTP& q) : qsize(0) {}
 	QueueTP& operator=(const QueueTP& q) { return *this; }
+public:
+	QueueTP(int qs = Q_SIZE);
+	~QueueTP();
+	bool isempty() const
+	{
+		return items == 0;
+	}
+	bool isfull() const
+	{
+		return items == qsize;
+	}
+	int queuecount() const
+	{
+		return items;
+	}
+	bool enqueue(const Item& item);
+	bool dequeue(Item& item);
 };
+template<class Item>
+QueueTP<Item>::QueueTP(int qs) : qsize(qs)
+{
+	front = rear = 0;
+	items = 0;
+}
+template <class Item>
+QueueTP<Item>::~QueueTP()
+{
+	Node* temp;
+	while (front != 0)
+	{
+		temp = front;
+		front = front->next;
+		delete temp;
+	}
+}
 #endif
 #pragma once
