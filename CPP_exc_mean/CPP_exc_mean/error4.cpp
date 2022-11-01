57c2f89d0263ed192b15161d790ecdc6867b480d
beonseong7 <beonseong7@naver.com>
2022-11-01 21:23:38 +0900

    2022.11.1 use object to exception

--- CPP_exc_mean/CPP_exc_mean/error4.cpp
+++ CPP_exc_mean/CPP_exc_mean/error4.cpp
@@ -28,5 +28,27 @@ int main()
 			cout << "다시 하십시오.\n";
 			continue;
 		}
+		catch (bad_geman& hg)
+		{
+			cout << hg.mesg();
+			cout << "Values used: " << hg.v1 << ","
+				<< hg.v2 << endl;
+			cout << "죄송합니다. 더 이상 진행할 수 없습니다.\n";
+			break;
+		}
 	}
+	cout << "프로그램을 종료합니다.\n";
+	return 0;
+}
+double hmean(double a, double b)
+{
+	if (a == -b)
+		throw bad_hmean(a, b);
+	return 2.0 * a * b / (a + b);
+}
+double gmean(double a, double b)
+{
+	if (a < 0 || b < 0)
+		throw bad_geman(a, b);
+	return std::sqrt(a * b);
 }
