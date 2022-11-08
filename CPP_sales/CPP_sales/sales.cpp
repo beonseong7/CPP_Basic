96072b2fe66a43bd73d642e9adafb0eac8ef4e65
beonseong7 <beonseong7@naver.com>
2022-11-08 20:07:27 +0900

    2022.11.8 exception,class,inheritance

---
+++ CPP_sales/CPP_sales/sales.cpp
@@ -0,0 +1,35 @@
+#include "sales.h"
+using std::string;
+
+Sales::bad_index::bad_index(int ix, const string& s)
+	:std::logic_error(s), bi(ix)
+{
+}
+Sales::Sales(int yy)
+{
+	year = yy;
+	for (int i = 0; i < MONTHS; ++i)
+		gross[i] = 0;
+}
+Sales::Sales(int yy, const double* gr, int n)
+{
+	year = yy;
+	int lim = (n < MONTHS) ? n : MONTHS;
+	int i;
+	for (i = 0; i < lim; ++i)
+		gross[i] = gr[i];
+	for (; i < MONTHS; ++i)
+		gross[i] = 0;
+}
+double Sales::operator[](int i) const
+{
+	if (i < 0 || i >= MONTHS)
+		throw bad_index(i);
+	return gross[i];
+}
+double& Sales::operator[](int i)
+{
+	if (i < 0 || i >= MONTHS)
+		throw bad_index(i);
+	return gross[i];
+}
