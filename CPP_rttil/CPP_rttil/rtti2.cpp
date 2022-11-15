f24a584f2e0b1fd51a91facb22bcc1a9a5a3bd9f
beonseong7 <beonseong7@naver.com>
2022-11-15 20:33:06 +0900

    2022.11.15 RTTI dynamic_cast

--- CPP_rttil/CPP_rttil/rtti2.cpp
+++ CPP_rttil/CPP_rttil/rtti2.cpp
@@ -30,5 +30,37 @@ private:
 public:
 	Magnificent(int h = 0, char cv = 'A') : Superb(h), ch(cv) {}
 	void Speak() const { cout << "나는 Magnificent 클래스이다!!!\n"; }
-	void Say() const { const << "내가 가지고 있는 문자는" << ch << "이고, 정수는 " << Value() << "이다.\n"; }
+	void Say() const { cout << "내가 가지고 있는 문자는" << ch << "이고, 정수는 " << Value() << "이다.\n"; }
 };
+Grand* GetOne();
+int main()
+{
+	srand(time(0));
+	Grand* pg;
+	Superb* ps;
+	for (int i = 0; i < 5; i++)
+	{
+		pg = GetOne();
+		cout << "지금 " << typeid(*pg).name() << "형을 처리하고 있습니다.\n";
+		pg->Speak();
+		if (ps = dynamic_cast<Superb*>(pg))
+			ps->Say();
+		if (typeid(Magnificent) == typeid(*pg))
+			cout << "그래, 너야말로 진짜 Magnificent 클래스이다,\n";
+	}
+	return 0;
+}
+Grand* GetOne()
+{
+	Grand* p;
+	switch (rand() % 3)
+	{
+	case 0:p = new Grand(rand() % 100);
+		break;
+	case 1:p = new Superb(rand() % 100);
+		break;
+	case 2:p = new Magnificent(rand() % 100, 'A' + rand() % 26);
+		break;
+	}
+	return p;
+}
