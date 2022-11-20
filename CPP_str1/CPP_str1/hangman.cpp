641a5f9d442f90a899247d3ff3b76f2718ae7170
beonseong7 <beonseong7@naver.com>
2022-11-20 15:18:15 +0900

    2022.11.20 hangman

--- CPP_str1/CPP_str1/hangman.cpp
+++ CPP_str1/CPP_str1/hangman.cpp
@@ -21,5 +21,33 @@ int main()
 	while (play == 'y')
 	{
 		string target = wordlist[std::rand() % NUM];
+		int length = target.length();
+		string attempt(length, '-');
+		string badchars;
+		int guesses = 6;
+		cout << "수수꼐끼 단어를 추측해 보십시오.\n"
+			<< length << "개의 문자로 이루어져 있습니다.\n"
+			<< "한 번에 한 문자씩 추측하십시오.\n"
+			<< "틀릴 수 있는 기회:" << guesses << "번\n";
+		cout << "추측하는 단어: " << attempt << endl;
+		while (guesses > 0 && attempt != target)
+		{
+			char letter;
+			cout << "문자를 추측하십시오: ";
+			cin >> letter;
+			if (badchars.find(letter) != string::npos
+				|| attempt.find(letter) != string::npos)
+			{
+				cout << "이미 추측한 문자입니다. 다시하십시오.\n";
+				continue;
+			}
+			int loc = target.find(letter);
+			if (loc == string::npos)
+			{
+				cout << "땡! 틀렸습니다.\n";
+				--guesses;
+				badchars += letter;
+			}
+		}
 	}
 }
