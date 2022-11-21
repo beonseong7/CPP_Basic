036a6e49e3a6e230b14d7d70f5bc1932a33a13df
beonseong7 <beonseong7@naver.com>
2022-11-21 21:37:07 +0900

    2022.11.21 hangman

--- CPP_str1/CPP_str1/hangman.cpp
+++ CPP_str1/CPP_str1/hangman.cpp
@@ -48,6 +48,35 @@ int main()
 				--guesses;
 				badchars += letter;
 			}
+			else
+			{
+				cout << "딩동댕! 맞았습니다.\n";
+				attempt[loc] = letter;
+				loc = target.find(letter, loc + 1);
+				while (loc != string::npos)
+				{
+					attempt[loc] = letter;
+					loc = target.find(letter, loc + 1);
+				}
+			}
+			cout << "추측하는 단어: " << attempt << endl;
+			if (attempt != target)
+			{
+				if (badchars.length() > 0)
+					cout << "틀리게 추측한 문자들: " << badchars << endl;
+
+					cout << "틀릴 수 있는 기회: " << guesses << "번\n";
+			}
 		}
+		if (guesses > 0)
+			cout << "그렇습니다. 그것이 수수께끼단어입니다.\n";
+		else
+			cout << "안타깝습니다. 수수께끼 단어는 " << target << "입니다.\n";
+
+		cout << "게임을 다시 하시겠습니까?<y/n>";
+		cin >> play;
+		play = tolower(play);
 	}
+	cout << "프로그램을 종료합니다.\n";
+	return 0;
 }
