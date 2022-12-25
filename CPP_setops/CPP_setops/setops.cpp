3a4b7d55545c56fec300ae5296f1b03166a794c0
beonseong7 <beonseong7@naver.com>
2022-12-25 20:57:00 +0900

    2022.12.25 associative container

---
+++ CPP_setops/CPP_setops/setops.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+#include<set>
+#include<algorithm>
+#include<iterator>
+
+int main()
+{
+	using namespace std;
+	const int N = 6;
+	string s1[N] = { "buffoon","thinkers", "for","heavy","can","for" };
+	string s2[N] = { "metal","any","food","elegant","deliver","for" };
+	set<string> A(s1, s1 + N);
+	set<string> B(s2, s2 + N);
+	ostream_iterator<string, char>out(cout, " ");
+	cout << "집합 A: ";
+	copy(A.begin(), A.end(), out);
+	cout << endl;
+	cout << "집합 B: ";
+	copy(B.begin(), B.end(), out);
+	cout << endl;
+}
